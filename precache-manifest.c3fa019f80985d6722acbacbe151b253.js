4589ec7b133b21f39b5b461c3bd04477dd483e21
Sennea <kmieszczak97@gmail.com>
2021-06-26 10:54:03 +0200

    Updates

---
+++ precache-manifest.c3fa019f80985d6722acbacbe151b253.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9ff196e8ffaf4aa9c186d0bf2b36d6f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c06eb75436bde853ecc4",
+    "url": "/portfolio/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "c06eb75436bde853ecc4",
+    "url": "/portfolio/static/js/2.540b5466.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.540b5466.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a3a63c2e4eeadcb37df6",
+    "url": "/portfolio/static/js/main.1f2712f9.chunk.js"
+  },
+  {
+    "revision": "f619aeccc94a0d089131",
+    "url": "/portfolio/static/js/runtime-main.075c0788.js"
+  },
+  {
+    "revision": "25e0c596b94c8dea032c2722be9a2cf2",
+    "url": "/portfolio/static/media/celluralAutomata.25e0c596.gif"
+  },
+  {
+    "revision": "b1b5e3afc83aaee25050f846cbe096d7",
+    "url": "/portfolio/static/media/celluralAutomata.b1b5e3af.png"
+  },
+  {
+    "revision": "1443d9a36cb7348f6ddb3c13cab4e689",
+    "url": "/portfolio/static/media/engeener.1443d9a3.png"
+  },
+  {
+    "revision": "ffa2b21a5fc184148ef9ed753a44b201",
+    "url": "/portfolio/static/media/engeener.ffa2b21a.gif"
+  },
+  {
+    "revision": "21aa93163feec94c0f2a6b59adb352d7",
+    "url": "/portfolio/static/media/feederer.21aa9316.png"
+  },
+  {
+    "revision": "a3c19f9481f45ca3944e4f7d76922711",
+    "url": "/portfolio/static/media/feederer.a3c19f94.gif"
+  },
+  {
+    "revision": "741f3072b231fc4c2c74141a456fcd15",
+    "url": "/portfolio/static/media/gameOfLife.741f3072.gif"
+  },
+  {
+    "revision": "e8d12887b8851a28c8d376349e66c6c5",
+    "url": "/portfolio/static/media/gameOfLife.e8d12887.png"
+  },
+  {
+    "revision": "37648c9cd4148bbdbf191b5cb00fdb87",
+    "url": "/portfolio/static/media/instagram.37648c9c.gif"
+  },
+  {
+    "revision": "c96664d7cba4b31718abc06c8f0a6674",
+    "url": "/portfolio/static/media/instagram.c96664d7.png"
+  },
+  {
+    "revision": "12f727c1e7900a36b22b14fd67f9417c",
+    "url": "/portfolio/static/media/mes.12f727c1.gif"
+  },
+  {
+    "revision": "ae924c6ba8ac18c2019dae3310da18c8",
+    "url": "/portfolio/static/media/mes.ae924c6b.JPG"
+  },
+  {
+    "revision": "16166faa3b30dfd4dc5955e866ce0f58",
+    "url": "/portfolio/static/media/photo.16166faa.jpg"
+  },
+  {
+    "revision": "8596cfb3e4b59bcb4e54901d678d61fe",
+    "url": "/portfolio/static/media/photo2.8596cfb3.jpg"
+  },
+  {
+    "revision": "c25cbb4826fa8aba436d856eef33bee1",
+    "url": "/portfolio/static/media/recipesApp.c25cbb48.png"
+  },
+  {
+    "revision": "c3a5feaf93a41a8dc6f81611af7542eb",
+    "url": "/portfolio/static/media/recipesApp.c3a5feaf.gif"
+  },
+  {
+    "revision": "08bd8155f299c9bd0d90949511622bf7",
+    "url": "/portfolio/static/media/toDoList.08bd8155.gif"
+  },
+  {
+    "revision": "8feff9df6855a02568481e2d217e3bc1",
+    "url": "/portfolio/static/media/toDoList.8feff9df.png"
+  },
+  {
+    "revision": "3c1af2c336b4ef681f59fcdfd58f2745",
+    "url": "/portfolio/static/media/weather.3c1af2c3.gif"
+  },
+  {
+    "revision": "5579fa93d0d61b52dc2d3e9a04dc57b9",
+    "url": "/portfolio/static/media/weather.5579fa93.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7e42a71995a12bc131b6a88ba3699fdd.js"
+  "/portfolio/precache-manifest.c3fa019f80985d6722acbacbe151b253.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
